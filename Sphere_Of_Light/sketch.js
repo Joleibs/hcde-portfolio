73adec96d22eed86b0a1456821fd7e81b3066066
Josh <josh.leibsohn@gmail.com>
2017-02-26 19:24:22 -0800

    added everything

--- Sphere_Of_Light/sketch.js
+++ Sphere_Of_Light/sketch.js
@@ -19,7 +19,7 @@ function draw() {
   directionalLight(timer, timer/2, b, dirX , dirY, 0.25); //interesting function I wanted to explore in the p5 library. 
   //First three arguments are R G B and moving relative to timer. b is randomized when timer runs out. Next three variables are X pos,Y pos, and Z pos
   ambientMaterial(250); //creates grey hue of ambient material for directional light interaction
-  sphere(100); //creates sphere vector for light to interact with
+  sphere (100); //creates sphere vector for light to interact with
   
   timer = timer + time //increase value of timer by time (1)
   
